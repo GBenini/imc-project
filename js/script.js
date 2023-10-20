93dee662bf47515416c77ce1abef2cd410477dc8
Guilherme Benini <guigabenini@gmail.com>
2023-10-20 14:16:06 -0300

    Add validating data

--- js/script.js
+++ js/script.js
@@ -12,6 +12,13 @@ form.onsubmit = (event) => {
     const weight = inputWeight.value;
     const height = inputHeight.value;
 
+    const showAlertError = notANumber(weight) || notANumber(height)
+
+    if(showAlertError) {
+        console.log('mostrar o alerta de erro')
+        return;
+    }
+
     const result = IMC(weight, height);
     const message = `O seu IMC é de ${result}`;
 
@@ -20,7 +27,9 @@ form.onsubmit = (event) => {
     Modal.open();
 }
 
-
+function notANumber(value) {
+    return isNaN(value) || value == ""
+}
 
 function IMC(weight, height) {
     return (weight / ((height / 100) ** 2)).toFixed(2);
