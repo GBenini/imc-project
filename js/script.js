1f3324c8948b50f6549fb263f7b8511e487459f9
Guilherme Benini <guigabenini@gmail.com>
2023-10-20 15:03:02 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -6,9 +6,6 @@ const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
 
-inputWeight.oninput = () => AlertError.close()
-inputHeight.oninput = () => AlertError.close()
-
 form.onsubmit = event => {
     event.preventDefault();
 
@@ -34,3 +31,6 @@ function displayResultMessage(result) {
     Modal.message.innerText = message;
     Modal.open();
 }
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
