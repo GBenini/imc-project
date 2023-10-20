d51d8a38ebf9cb772f4b07a3ac2fdcd5b1741f0c
Guilherme Benini <guigabenini@gmail.com>
2023-10-20 15:01:24 -0300

    Clean Code

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 import { Modal } from './modal.js'
 import { AlertError } from './alert-error.js'
-import { IMC, notANumber } from './utils.js'
+import { calculateIMC, notANumber } from './utils.js'
 
 const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
 
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
 
-
-form.onsubmit = (event) => {
+form.onsubmit = event => {
     event.preventDefault();
 
     const weight = inputWeight.value;
     const height = inputHeight.value;
 
-    const showAlertError = notANumber(weight) || notANumber(height)
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-    if(showAlertError) {
+    if(weightOrHeightIsNotANumber) {
         AlertError.open()
         return;
     }
 
     AlertError.close()
 
-    const result = IMC(weight, height);
+    const result = calculateIMC(weight, height); 
+    displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
     const message = `O seu IMC é de ${result}`;
 
     Modal.message.innerText = message;
-
     Modal.open();
 }
-
