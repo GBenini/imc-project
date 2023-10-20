42655736f4b7e198b223f9b460d0834bd42665c9
Guilherme Benini <guigabenini@gmail.com>
2023-10-20 14:23:31 -0300

    Add Alert Error

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
 
 const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
@@ -15,9 +16,11 @@ form.onsubmit = (event) => {
     const showAlertError = notANumber(weight) || notANumber(height)
 
     if(showAlertError) {
-        console.log('mostrar o alerta de erro')
+        AlertError.open()
         return;
     }
+
+    AlertError.close()
 
     const result = IMC(weight, height);
     const message = `O seu IMC é de ${result}`;
