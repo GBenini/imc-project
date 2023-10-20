9424ab9a3964790ce377686ceb70c15e272ab0c2
Guilherme Benini <guigabenini@gmail.com>
2023-10-20 09:19:51 -0300

    JS Code Review

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
-const form = document.querySelector('form')
-const inputWeight = document.querySelector('#weight')
-const inputHeight = document.querySelector('#height')
+const form = document.querySelector('form');
+const inputWeight = document.querySelector('#weight');
+const inputHeight = document.querySelector('#height');
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+const Modal = {
 
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
 
-
-form.onsubmit = (event) => {
-    event.preventDefault()
-
-    const weight = inputWeight.value
-    const height = inputHeight.value
-
-    const result = IMC(weight, height)
-    const message = `O seu IMC é de ${result}`
-
-    modalWrapper.classList.add('open')
-    modalMessage.innerText = message
+    open() {
+        Modal.wrapper.classList.add('open');
+    },
+    close() {
+        Modal.wrapper.classList.remove('open');
+    },
 }
 
-modalBtnClose.onclick = () => {
-    modalWrapper.classList.remove('open')
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const weight = inputWeight.value;
+    const height = inputHeight.value;
+
+    const result = IMC(weight, height);
+    const message = `O seu IMC é de ${result}`;
+
+    Modal.message.innerText = message;
+
+    Modal.open();
+}
+
+Modal.buttonClose.onclick = () => {  
+    Modal.close();
 }
 
 function IMC(weight, height) {
-    return (weight / ((height / 100) ** 2)).toFixed(2)
+    return (weight / ((height / 100) ** 2)).toFixed(2);
 }
